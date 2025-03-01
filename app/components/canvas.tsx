cd7b2b3f1e827536b56db86234f8c0640e8d017f
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-03-01 12:16:46 -0500

    2 divs

---
+++ app/components/canvas.tsx
@@ -0,0 +1,12 @@
+
+
+import styles from './styles/canvas.module.css';
+
+
+export default function CanvasWindow() {
+    return (
+        <div className={styles.container}>
+            <h1>Canvas</h1>
+        </div>
+    );
+}
---
+++ app/components/sidebar.tsx
@@ -0,0 +1,10 @@
+
+import styles from './styles/sidebar.module.css';
+
+export default function SideBar(){
+    return (
+        <div className={styles.container}>
+        <h1>SideBar</h1>
+        </div>
+    );
+}
